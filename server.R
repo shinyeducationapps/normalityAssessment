bf21761a0680d3e244ff24adf135ba5ddc02b643
Casement <ccasement@univ.fairfield.local>
2020-06-03 10:45:57 -0400

    Initial commit

---
+++ server.R
@@ -0,0 +1,328 @@
+
+library(shiny)
+library(ggplot2); theme_set(theme_grey(20))
+library(plyr)
+library(dplyr)
+
+
+shinyServer(function(session, input, output){
+  
+  # create and initialize reactive stuff
+  bag <- reactiveValues()
+  
+  hist_bins_rorsch <- reactive(input$hist_bins_rorsch)
+  hist_bins_rorsch_both <- reactive(input$hist_bins_rorsch_both)
+  hist_bins_lineup <- reactive(input$hist_bins_lineup)
+  hist_bins_lineup_both <- reactive(input$hist_bins_lineup_both)
+  d_hist_bins_rorsch <- debounce(hist_bins_rorsch, 1000)          # 1000 = 1 second delay
+  d_hist_bins_rorsch_both <- debounce(hist_bins_rorsch_both, 1000)
+  d_hist_bins_lineup <- debounce(hist_bins_lineup, 1000)
+  d_hist_bins_lineup_both <- debounce(hist_bins_lineup_both, 1000)
+  
+  
+  # function for density curves
+  make_density_df <- function(df) {
+    x_grid <- seq(min(df$Data), max(df$Data), length.out = 1e3)
+    
+    ddply(df, "samp", function(dframe) {
+      data.frame(
+        Data = x_grid,
+        norm_density = dnorm(x_grid, mean(dframe$Data), sd(dframe$Data))
+      )
+    })
+  }
+  
+  
+  
+  ##### rorschach plots
+  #######################################################
+  
+  # generate data
+  observeEvent(input$make_rorsch_plots, {
+
+    if(input$pop_dist == "basic"){
+      # pop. distribution to randomly sample from
+      dist_random <- ifelse(input$pop_shape == "Normal", rnorm, rbeta)
+      
+      # parameters of pop. distribution
+      params <- switch(input$pop_shape,
+        "Normal" = c(0, 1),
+        "Left Skewed" = c(5, 2),
+        "Very Left Skewed" = c(10, 2),
+        "Severely Left Skewed" = c(50, 2),
+        "Right Skewed" = c(2, 5),
+        "Very Right Skewed" = c(2, 10),
+        "Severely Right Skewed" = c(2, 50)
+      )
+    } else {
+      # pop. distribution to randomly sample from
+      dist_random <- switch(input$pop_fam,
+        "Normal" = rnorm, "t" = rt, "F" = rf, "Uniform" = runif, 
+        "Chi-square" = rchisq, "Exponential" = rexp, "Gamma" = rgamma, 
+        "Beta" = rbeta
+      )
+      
+      # parameters of pop. distribution
+      params <- switch(input$pop_fam,
+        "Normal" = c(0, 1),
+        "t" = input$t_df,
+        "F" = c(input$f_df1, input$f_df2),
+        "Uniform" = c(0, 1),
+        "Chi-square" = input$chisq_df,
+        "Exponential" = input$exp_rate,
+        "Gamma" = c(input$gamma_shape, input$gamma_rate),
+        "Beta" = c(input$beta_shape1, input$beta_shape2)
+      )
+    }
+    
+    
+    ### randomly sample data
+    ##################################
+    samples_rorsch <- 1:input$n_plots_rorsch
+    
+    datasets <- if(input$pop_fam %in% c("t", "Chi-square", "Exponential")){
+      lapply(seq_along(samples_rorsch), function(samples_rorsch) {
+        data.frame(
+          Data = dist_random(input$sample_size, params), 
+          samp = rep(samples_rorsch, each = input$sample_size), 
+          stringsAsFactors = FALSE)
+      })
+    } else {
+      lapply(seq_along(samples_rorsch), function(samples_rorsch) {
+        data.frame(
+          Data = dist_random(input$sample_size, params[1], params[2]), 
+          samp = rep(samples_rorsch, each = input$sample_size), 
+          stringsAsFactors = FALSE)
+      })
+    }
+    
+    bag$all_data_rorsch <- do.call(rbind, datasets)
+
+    
+    ### make rorschach plots
+    ##################################
+    if(input$rorsch_plot_type == "rorsch_qq") {
+      output$qq_rorsch <- renderPlot(
+        ggplot(bag$all_data_rorsch, aes(sample = Data)) +
+          stat_qq() +
+          stat_qq_line() +
+          facet_wrap(~ samp) +
+          labs(x = "Theoretical Quantile", y = "Sample Quantile") +
+          theme(strip.text.x = element_text(margin = margin(2, 0, 2, 0)))
+      )
+    } else if (input$rorsch_plot_type == "rorsch_hist") {
+      output$hist_rorsch <- renderPlot({
+        hist_r <- ggplot(bag$all_data_rorsch, aes(x = Data)) +
+          geom_histogram(aes(y = stat(density)), bins = d_hist_bins_rorsch()) +
+          facet_wrap(~ samp) +
+          labs(x = "Data Value", y = "Density") +
+          theme(strip.text.x = element_text(margin = margin(2, 0, 2, 0)))
+        
+        if(input$add_density_rorsch) {
+          density_df <- make_density_df(bag$all_data_rorsch)
+          
+          hist_r <- hist_r +
+            geom_line(aes(y = norm_density), data = density_df, 
+              color = "red", lwd = 1.1
+            )
+        }
+        
+        hist_r
+      })
+    } else {
+      output$qq_rorsch_both <- renderPlot(
+        ggplot(bag$all_data_rorsch, aes(sample = Data)) +
+          stat_qq() +
+          stat_qq_line() +
+          facet_wrap(~ samp) +
+          labs(x = "Theoretical Quantile", y = "Sample Quantile") +
+          theme(strip.text.x = element_text(margin = margin(2, 0, 2, 0)))
+      )
+      
+      output$hist_rorsch_both <- renderPlot({
+        hist_r <- ggplot(bag$all_data_rorsch, aes(x = Data)) +
+          geom_histogram(aes(y = stat(density)), bins = d_hist_bins_rorsch_both()) +
+          facet_wrap(~ samp) +
+          labs(x = "Data Value", y = "Density") +
+          theme(strip.text.x = element_text(margin = margin(2, 0, 2, 0)))
+        
+        if(input$add_density_rorsch_both) {
+          density_df <- make_density_df(bag$all_data_rorsch)
+          
+          hist_r <- hist_r +
+            geom_line(aes(y = norm_density), data = density_df, 
+              color = "red", lwd = 1.1
+            )
+        }
+        
+        hist_r
+      })
+    }
+  })
+  
+  
+  observeEvent(input$data_file, {
+      
+    req(input$data_file)
+      
+    bag$user_data_df <- read.csv(input$data_file$datapath, 
+      header = input$file_header, sep = input$file_separator
+    )
+    
+    # remove non-numeric variables from uploaded dataset
+    bag$user_data_df <- select_if(bag$user_data_df, is.numeric)
+
+    updateSelectInput(session, "select_var", "Variable of Interest",
+      choices = colnames(bag$user_data_df)
+    )
+  })
+  
+
+  ### print data in tabular form
+  #################################################
+  
+  output$data_table <- renderTable({
+    
+    # store data either uploaded or manually input by user
+    if(input$input_type == "upload") {
+      
+      # print data frame showing either all or only 10 rows
+      if(input$file_display == "first_10") {
+        return(head(bag$user_data_df, n = 10))
+      } else {
+        return(bag$user_data_df)
+      }
+    } else if(input$input_type == "manually") {
+      
+      # convert user-input data to numeric vector
+      user_data <- input$user_data %>% 
+        strsplit(split = "\n|,") %>%  # split if see comma or new line
+        unlist() %>%
+        gsub("[^0-9\\.]", "", .) %>%  # only keep integers and decimals
+        as.numeric() %>%
+        na.omit()    # remove any NAs
+      
+      (bag$user_data_df <- data.frame(Data = user_data))
+    }
+  })
+  
+  
+
+  ##### lineup plots
+  #######################################################
+  
+  observeEvent(input$make_lineup_plots, {
+  
+    if(input$input_type == "upload") {
+      if(ncol(bag$user_data_df) > 1) {
+        final_user_data <- data.frame(Data = bag$user_data_df[, input$select_var])
+      } else {
+        final_user_data <- data.frame(Data = bag$user_data_df[, 1])
+      }
+    } else if(input$input_type == "manually") {
+      final_user_data <- bag$user_data_df
+    }
+    
+    # count how many numbers input by user; excludes extra . (for missing data)
+    #  and other non-numeric characters
+    sample_size_user <- nrow(final_user_data)
+
+    # randomly permute plot indices
+    plot_spots <- sample(input$n_plots_lineup)
+
+    # simulate data -- mean and SD are mean and SD of user's data
+    datasets_lineup <- lapply(1:(input$n_plots_lineup - 1), function(Data) {
+      data.frame(Data = rnorm(sample_size_user, mean(final_user_data$Data), 
+        sd(final_user_data$Data)
+      ))
+    })
+    sim_data_lineup <- do.call(rbind, datasets_lineup)
+    
+
+    # combine user data and simulated data
+    bag$all_data_lineup <- rbind(final_user_data, sim_data_lineup)
+    bag$all_data_lineup$samp <- rep(plot_spots, each = sample_size_user)
+    
+    
+
+    ### make lineup plots
+    ##################################
+    if(input$lineup_plot_type == "lineup_qq") {
+      output$qq_lineup <- renderPlot({
+        ggplot(bag$all_data_lineup, aes(sample = Data)) +
+          stat_qq() +
+          stat_qq_line() +
+          facet_wrap(~ samp) +
+          labs(x = "Theoretical Quantile", y = "Sample Quantile") +
+          theme(strip.text.x = element_text(margin = margin(2, 0, 2, 0)))
+      })
+    } else if (input$lineup_plot_type == "lineup_hist") {
+      output$hist_lineup <- renderPlot({
+        hist_l <- ggplot(bag$all_data_lineup, aes(x = Data)) +
+          geom_histogram(aes(y = stat(density)), bins = d_hist_bins_lineup()) +
+          facet_wrap(~ samp) +
+          labs(x = "Data Value", y = "Density") +
+          theme(strip.text.x = element_text(margin = margin(2, 0, 2, 0)))
+        
+        if(input$add_density_lineup) {
+          density_df <- make_density_df(bag$all_data_lineup)
+          
+          hist_l <- hist_l +
+            geom_line(aes(y = norm_density), data = density_df, 
+                color = "red", lwd = 1.1
+            )
+        }
+        
+        hist_l
+      })
+    } else {
+      output$qq_lineup_both <- renderPlot({
+        ggplot(bag$all_data_lineup, aes(sample = Data)) +
+          stat_qq() +
+          stat_qq_line() +
+          facet_wrap(~ samp) +
+          labs(x = "Theoretical Quantile", y = "Sample Quantile") +
+          theme(strip.text.x = element_text(margin = margin(2, 0, 2, 0)))
+      })
+
+      output$hist_lineup_both <- renderPlot({
+        hist_l <- ggplot(bag$all_data_lineup, aes(x = Data)) +
+          geom_histogram(aes(y = stat(density)), bins = d_hist_bins_lineup_both()) +
+          facet_wrap(~ samp) +
+          labs(x = "Data Value", y = "Density") +
+          theme(strip.text.x = element_text(margin = margin(2, 0, 2, 0)))
+        
+        if(input$add_density_lineup_both) {
+          density_df <- make_density_df(bag$all_data_lineup)
+          
+          hist_l <- hist_l +
+            geom_line(aes(y = norm_density), data = density_df, 
+              color = "red", lwd = 1.1
+            )
+        }
+        
+        hist_l
+      })
+    }
+    
+    
+    # reset identify plot output
+    output$user_plot_number <- renderText("")
+  })
+  
+  
+  # ### add density curves to line-up histograms
+  # output$hist_lineup <- renderPlot({
+  # 
+  # 
+  # })
+  
+    
+  # print plot number associated w/ user's data
+  observeEvent(input$identify, {
+    output$user_plot_number <- renderText(bag$all_data_lineup$samp[1])
+  })
+})
+
+
+
---
+++ ui.R
@@ -0,0 +1,357 @@
+
+library(shiny)
+
+shapes <- list("Severely Left Skewed", "Very Left Skewed", "Left Skewed", 
+  "Normal", "Right Skewed", "Very Right Skewed", "Severely Right Skewed"
+)
+
+families <- list("Normal", "t", "F", "Uniform", "Chi-square", "Exponential", 
+  "Gamma", "Beta"
+)
+
+navbarPage("Normality Assessment",
+
+  ##### rorschach tabpanel
+  #####################################################
+  tabPanel("Explore Simulated Data", icon = icon("bolt"),
+    
+    column(3,
+      wellPanel(
+        radioButtons("pop_dist", label = "Distribution of Population",
+          choices = c("Basic: Select Shape" = "basic", 
+            "Advanced: Select Specific Distribution" = "advanced")
+        ),
+        
+        conditionalPanel(
+          condition = "input.pop_dist == 'basic'",
+          selectInput("pop_shape", label = "Shape", 
+            choices = shapes, selected = "Normal", width = "75%")
+        ),
+        
+        conditionalPanel(
+          condition = "input.pop_dist == 'advanced'",
+          selectInput("pop_fam", label = "Family",
+            choices = families, selected = "Normal", width = "50%"),
+
+          conditionalPanel(
+            condition = "input.pop_fam == 't'",
+            numericInput("t_df", label = "DF", value = 10, width = "50%")
+          ),
+
+          conditionalPanel(
+            condition = "input.pop_fam == 'F'",
+            
+            splitLayout(
+              numericInput("f_df1", label = "DF 1", value = 5),
+              numericInput("f_df2", label = "DF 2", value = 5)
+            )
+          ),
+          
+          conditionalPanel(
+            condition = "input.pop_fam == 'Chi-square'",
+            numericInput("chisq_df", label = "DF", value = 5, width = "50%")
+          ),
+          
+          conditionalPanel(
+            condition = "input.pop_fam == 'Exponential'",
+            numericInput("exp_rate", label = "Rate", value = 5, width = "50%")
+          ),
+          
+          conditionalPanel(
+            condition = "input.pop_fam == 'Gamma'",
+            
+            splitLayout(
+              numericInput("gamma_shape", label = "Shape", value = 5),
+              numericInput("gamma_rate", label = "Rate (or Scale ??)", value = 5)   # explain rate vs. scale
+            )
+          ),
+          
+          conditionalPanel(
+            condition = "input.pop_fam == 'Beta'",
+            
+            splitLayout(
+              numericInput("beta_shape1", label = "Shape 1", value = 5),
+              numericInput("beta_shape2", label = "Shape 2", value = 5)
+            )
+          )
+        ),
+        
+        splitLayout(
+          numericInput("sample_size", label = "Sample Size", value = 20, 
+            width = "100%"),
+          
+          numericInput("n_plots_rorsch", label = "Number of Plots", value = 12, 
+            width = "100%")
+        ),
+        
+        radioButtons("rorsch_plot_type", label = "Plot Type",
+          choices = c("Normal QQ Plot" = "rorsch_qq",
+            "Histogram" = "rorsch_hist",
+            "Both" = "rorsch_qq_and_hist"
+          )
+        ),
+        
+        actionButton("make_rorsch_plots", label = "Make Plots", 
+          class = "btn btn-primary"),
+        br()
+      )
+    ),
+    
+    column(8,
+      h4("Look for any expected and unexpected characteristics in the plots 
+          below."),
+      h4("This will help develop a better understanding of the natural 
+          variability in data that is randomly generated from a particular 
+          distribution."),
+
+      conditionalPanel(
+        condition = "input.rorsch_plot_type == 'rorsch_qq'",
+        
+        plotOutput("qq_rorsch", height = 400)
+      ),
+  
+      conditionalPanel(
+        condition = "input.rorsch_plot_type == 'rorsch_hist'",
+        
+        plotOutput("hist_rorsch", height = 400),
+        
+        column(3,
+          checkboxInput("add_density_rorsch", "Add Normal Density Curves",
+            value = FALSE)
+        ),
+        
+        column(9,
+          sliderInput("hist_bins_rorsch", label = "Number of Bins", min = 5,
+            max = 50, value = 30, width = "50%")
+        )
+      ),
+  
+      conditionalPanel(
+        condition = "input.rorsch_plot_type == 'rorsch_qq_and_hist'",
+        column(6, plotOutput("qq_rorsch_both", height = 400)),
+        column(6, plotOutput("hist_rorsch_both", height = 400))
+      )
+    ),
+      
+    column(12, offset = 1,
+      conditionalPanel(
+        condition = "input.rorsch_plot_type == 'rorsch_qq_and_hist'",
+        
+        column(1, offset = 7,
+          checkboxInput("add_density_rorsch_both", "Add Normal Density 
+                Curves", value = FALSE)
+        ),
+        
+        column(4,
+          sliderInput("hist_bins_rorsch_both", label = "Number of Bins", 
+            min = 5, max = 50, value = 30, width = "50%")
+        )
+      )
+    ) #,
+  
+    # column(2, 
+    #   actionButton("make_new_rorsch", label = "Generate New Plots", 
+    #     class = "btn btn-primary"),
+    #   style = "padding-top: 50px;"
+    # )
+  ),
+
+  
+  ##### lineup tabpanel
+  #####################################################
+  tabPanel("Include Your Data", icon = icon("table"),
+    
+    tabsetPanel(type = "tabs",
+      tabPanel("Input Data",
+        
+        br(),
+        
+        p("You can input data two ways: (1) manually, or (2) by uploading a CSV 
+          file."),
+        
+        column(3,
+          wellPanel(
+            radioButtons("input_type", "How to input data", 
+              choices = c("Manually" = "manually", "Upload File" = "upload"),
+              selected = "manually"
+            ),
+            
+            tags$hr(),
+            
+            conditionalPanel(
+              condition = "input.input_type == 'manually'",
+              
+              textAreaInput("user_data", 
+                label = "Either copy and paste a column from a spreadsheet (such 
+                  as Excel) or type values in the box below, separating each by a 
+                  comma.",
+                placeholder = "0, 2, 8"
+              )
+            ),
+            
+            conditionalPanel(
+              condition = "input.input_type == 'upload'",
+              
+              fileInput("data_file", label = "Select a CSV file.", 
+                multiple = FALSE, accept = ".csv"),
+              
+              tags$hr(),
+              
+              checkboxInput("file_header", "Header: check if the first row 
+                contains variable (column) names", value = TRUE),
+              
+              radioButtons("file_separator", "Value Separator", 
+                choices = c(Comma = ",", Semicolon = ";", Tab = "\t"), 
+                selected = ","),
+              
+              radioButtons("file_display", "Number of rows to display", 
+                choices = c("First 10" = "first_10", "All" = "all"), 
+                selected = "first_10")
+            )
+          )
+        ),
+
+        column(8, tableOutput("data_table"))
+      ),
+      
+      tabPanel("Make Plots",
+        
+        br(),
+        
+        column(3,
+          wellPanel(
+            
+            conditionalPanel(
+              condition = "input.input_type == 'upload'",
+              
+              selectInput("select_var", "Variable of Interest", 
+                choices = "", selected = "", width = "75%")
+            ),
+            
+            numericInput("n_plots_lineup", label = "Number of Plots", value = 12, 
+              width = "50%"),
+        
+            radioButtons("lineup_plot_type", label = "Plot Type",
+              choices = c("Normal QQ Plot" = "lineup_qq",
+              "Histogram" = "lineup_hist",
+              "Both" = "lineup_qq_and_hist"
+              )
+            ),
+        
+            actionButton("make_lineup_plots", label = "Make Plots", 
+              class = "btn btn-primary")
+          )
+        ),
+        
+        column(9,
+          column(10,
+            h4("One of the plots below corresponds to your data, while the 
+              remaining plots correspond to data sets randomly generated from the 
+              standard normal distribution, N(0,1)."),
+            h4("Can you successfully determine which plot corresponds to your 
+              data?"),
+            h4("- If yes, then it is reasonable to conclude your data did not come 
+              from a normal population."),
+            h4("- If no, then it is plausible your data came from a normal 
+              population.")
+          ),
+          
+          column(2, 
+            h4("My Plot:"),
+            verbatimTextOutput("user_plot_number", placeholder = TRUE),
+            tags$head(tags$style(type="text/css", "#user_plot_number{max-width: 60px;}")),
+            align = "center",
+
+            actionButton("identify", label = "Identify My Plot", 
+              class = "btn btn-primary")
+          ),
+          
+          conditionalPanel(
+            condition = "input.lineup_plot_type == 'lineup_qq'",
+            plotOutput("qq_lineup", height = 400)
+          ),
+          
+          conditionalPanel(
+            condition = "input.lineup_plot_type == 'lineup_hist'",
+            
+            column(12,
+              plotOutput("hist_lineup", height = 400)
+            ),
+            
+            column(3,
+              checkboxInput("add_density_lineup", "Add Normal Density Curves",
+                value = FALSE)
+            ),
+            
+            column(9,
+              sliderInput("hist_bins_lineup", label = "Number of Bins", min = 5,
+                max = 50, value = 30, width = "50%")
+            )
+          ),
+          
+          conditionalPanel(
+            condition = "input.lineup_plot_type == 'lineup_qq_and_hist'",
+            column(6, plotOutput("qq_lineup_both", height = 400)),
+            column(6, plotOutput("hist_lineup_both", height = 400))
+          )
+        ),
+        
+        column(12, offset = 1,
+          conditionalPanel(
+            condition = "input.lineup_plot_type == 'lineup_qq_and_hist'",
+            
+            column(1, offset = 7,
+              checkboxInput("add_density_lineup_both", "Add Normal Density 
+                Curves", value = FALSE)
+            ),
+            
+            column(4,
+              sliderInput("hist_bins_lineup_both", label = "Number of Bins", 
+                min = 5, max = 50, value = 30, width = "50%")
+            )
+          )
+        )
+      )
+    )
+  ),
+  
+  
+  ##### about tabpanel
+  #####################################################
+  tabPanel("About", icon = icon("info-circle"),
+    
+    column(4,
+      
+      h4("Out of respect for the blind review process, the author will update 
+         this section after the completion of the process."),
+      br(),
+      
+      # information about the developer
+      h4("Developer"),
+      p("This tool was developed by Anonymous."),
+      br(),
+      
+      # information about the app
+      h4("About the App"),
+      p("This tool creates normal quantile-quantile (QQ) plots and histograms for 
+        assessing normality."),
+      br(),
+      
+      # contact info
+      h4("Contact"),
+      p("Email: Anonymous"),
+      br(),
+      br(),
+      
+      # copyright statement
+      p("Copyright (c) 2019-2020 by Anonymous."),
+      p("The license statement can be found", 
+        a("here.", href = "https://opensource.org/licenses/MIT", target = "_blank")
+      )
+    )
+  )
+)
+
+
+
+    
